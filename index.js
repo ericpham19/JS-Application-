047788198efd532b6739d9d7bc3cd7cbaf03ae95
Duy Pham <ericpham19@outlook.com>
2022-07-31 18:49:27 +1000

    starting on js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 console.log ("hello");
 
-console.log ("hello");
-console.log ("hello");
-console.log ("hello");
+let submitButton = document.getElementById("submitBtn");
+let temperature = doucment.getElementById("temp");
+let weatherIcon = document.getElementById("icon")
+let weatherInfo = document.getElementByClass("weather-description ")
+
+let url= `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=e11e508720aa8c243305040e3bf2432a`
+
