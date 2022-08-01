8cafce74ff1efff13f2343fe0e3eb4226f836237
Duy Pham <ericpham19@outlook.com>
2022-08-01 12:27:37 +1000

    weather API working

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-console.log ("hello");
+
 
 let submitButton = document.getElementById("submitBtn");
-let temperature = doucment.getElementById("temp");
+let temperature = document.getElementById("temp");
 let weatherIcon = document.getElementById("icon")
-let weatherInfo = document.getElementByClass("weather-description ")
+let weatherInfo = document.getElementById("weather-description")
+let cityName = document.getElementById("city-result");
 
-let url= `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=e11e508720aa8c243305040e3bf2432a`
 
+
+
+function findWeather(){
+    let textField = document.getElementById("city");
+    let url= `https://api.openweathermap.org/data/2.5/forecast?q=${textField.value}&units=metric&appid=33d7061a211b97265abda12a2d87a56e`
+
+    console.log(url)
+    fetch (url).then(response => response.json())
+    .then(data =>{
+        console.log (data)
+        temperature.innerText = data.list[0].main.temp
+        weatherInfo.innerText = data.list[0].weather[0].description
+        cityName.innerText = data.city.name;
+        iconCode = data.list[0].weather[0].icon;
+        iconUrl= 'https://api.openweathermap.org/img/w/'+ iconCode;
+        weatherIcon.src = iconUrl;
+    
+    })
+    .catch (error => console.log("error")) 
+}
